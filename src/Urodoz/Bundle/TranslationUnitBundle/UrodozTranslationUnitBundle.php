605756422a3deed7e1d095a71e46ac087665a38a
Albert Lacarta <urodoz@gmail.com>
2013-11-01 13:19:16 +0100

    Added base code for bundle

---
+++ src/Urodoz/Bundle/TranslationUnitBundle/UrodozTranslationUnitBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the UrodozTranslationUnit bundle.
+ *
+ * (c) Albert Lacarta <urodoz@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Urodoz\Bundle\TranslationUnitBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UrodozTranslationUnitBundle extends Bundle
+{
+}
